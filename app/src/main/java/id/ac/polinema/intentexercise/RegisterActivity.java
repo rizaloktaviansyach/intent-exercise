ae4c204368551917b2237f80222276d67c363c3a
rizaloktavian <rizaloktaviansyach99@gmail.com>
2020-02-27 19:35:35 +0700

    membuat class user

--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -3,12 +3,23 @@ package id.ac.polinema.intentexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    public static String USER_KEY = "user";
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText homepageInput;
+    private EditText aboutInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        passwordInput = findViewById(R.id.text_password);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
     }
 }
---
+++ app/src/main/java/id/ac/polinema/intentexercise/model/User.java
@@ -0,0 +1,99 @@
+package id.ac.polinema.intentexercise.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+    private String fullname, email, password,confirm_password, homepage, about;
+
+    public String getConfirm_password() {  
+        return confirm_password;
+    }
+
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
+
+    public User(String fullname, String email, String password, String confirm_password, String homepage, String about) {
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+        this.confirm_password = confirm_password;
+        this.homepage = homepage;
+        this.about = about;
+    }
+
+    protected User(Parcel in) {
+        fullname = in.readString();
+        email = in.readString();
+        password = in.readString();
+        homepage = in.readString();
+        about = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(fullname);
+        dest.writeString(email);
+        dest.writeString(password);
+        dest.writeString(homepage);
+        dest.writeString(about);
+    }
+}
